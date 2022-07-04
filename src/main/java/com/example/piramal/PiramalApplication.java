9913bd49bef48e255463d54ad0e90a79c6e4f2b5
abhijitKharade <abhi.k1808@gmail.com>
2022-07-04 15:45:13 +0530

    initilize

---
+++ src/main/java/com/example/piramal/PiramalApplication.java
@@ -0,0 +1,13 @@
+package com.example.piramal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiramalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiramalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/piramal/PiramalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.piramal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiramalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
